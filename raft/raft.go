99c756496d134213f8b299b32dea5ff856df11e3
Seth Ammons <seth@sendgrid.com>
2016-03-12 08:45:55 -0800

    WIP: working on elections

--- raft/raft.go
+++ raft/raft.go
@@ -3,15 +3,60 @@ package raft
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
 	"net/rpc"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
+)
+
+type state int
+
+// TriggerElectionTimeoutCap is the max time when a node decides to run for office
+// var TriggerElectionTimeoutCap = 150 * time.Millisecond
+var TriggerElectionTimeoutCap = 7 * time.Second
+
+// TriggerElectionTimeoutMin is the minimum amount of time before a node runs for office
+// var TriggerElectionTimeoutMin = 75 * time.Millisecond
+var TriggerElectionTimeoutMin = 2 * time.Second
+
+// ElectionTimeout the time that a node will wait for votes on its nomination
+// This should be about an order of magnitude greater than the TriggerElectionTimeoutCap
+var ElectionTimeout = TriggerElectionTimeoutCap * 10
+
+// ErrElectionTimeout is returned when an election times out
+var ErrElectionTimeout = fmt.Errorf("election timed out")
+
+const (
+	// NullVote represents when no vote has been cast by a node
+	NullVote = -1
+
+	// FollowerState is the default state for any node
+	FollowerState = 0
+
+	// CandidateState is when the node is running for office to be leader
+	CandidateState = 1
+
+	// LeaderState lets a node know it is a leader
+	LeaderState = 2
 )
 
 // Debug, when set, will give detailed logs of what is going on
 var Debug bool
+
+func init() {
+	// TODO: find that article that said not to do this, and let package caller set seed
+	rand.Seed(time.Now().UnixNano())
+}
+
+// LogEntry represents a raft log entry
+type LogEntry struct {
+	Term  int
+	Index int
+	Data  []byte
+}
 
 // Reply states if the previous message was applied
 type Reply struct {
@@ -20,24 +65,24 @@ type Reply struct {
 
 // Cluster is our cluster
 type Cluster struct {
+	sync.Mutex
 	// curLeader is an index in Nodes
 	curLeader int
 	Nodes     []*Node
 }
 
 // CurLeader returns the leader node
-// TODO: handle when there is no leader
-func (c Cluster) CurLeader() Node {
-	return Node{
-		Port:    c.Nodes[c.curLeader].Port,
-		Cluster: c.Nodes[c.curLeader].Cluster,
-		ID:      c.Nodes[c.curLeader].ID,
-		Log:     c.Nodes[c.curLeader].Log,
+func (c *Cluster) CurLeader() *Node {
+	// TODO: should use atomic for leaderPoll? function? channel?
+	for c.curLeader == NullVote {
+		debug("no leader, polling...")
+		<-time.After(500 * time.Millisecond)
 	}
+	return c.Nodes[c.curLeader]
 }
 
 // NewCluster returns a cluster of a given size
-func NewCluster(count int) (Cluster, error) {
+func NewCluster(count int) (*Cluster, error) {
 	nodes := make([]*Node, count)
 	var err error
 	for i := 0; i < count; i++ {
@@ -45,26 +90,17 @@ func NewCluster(count int) (Cluster, error) {
 		nodes[i], err = NewNode(i)
 		if err != nil {
 			log.Println("unable to create node", err)
-			return Cluster{}, err
+			return nil, err
 		}
 		debug("node created!")
 	}
 
-	cluster := Cluster{Nodes: nodes, curLeader: 0}
+	cluster := &Cluster{Nodes: nodes, curLeader: NullVote}
 	for _, node := range cluster.Nodes {
 		node.Cluster = cluster
 	}
 
 	return cluster, err
-}
-
-// Node is a single running instance
-type Node struct {
-	ID   int
-	Port int
-	Log  []LogEntry
-
-	Cluster Cluster
 }
 
 // Send publishes a message to the cluster
@@ -86,6 +122,44 @@ func (c *Cluster) Send(msg []byte) error {
 	return nil
 }
 
+// StopNode turns off a node
+func (c *Cluster) StopNode(id int) error {
+	for _, node := range c.Nodes {
+		if node.ID == id {
+			node.stop()
+			return nil
+		}
+	}
+	return nil
+}
+
+// Node is a single running instance
+type Node struct {
+	mtx sync.Mutex
+
+	State       state
+	ID          int
+	Port        int
+	Log         []LogEntry
+	CurrentTerm int
+	VotedFor    int
+	CommitIndex int
+	LastApplied int
+	NextIndex   int
+	MatchIndex  int
+
+	Cluster *Cluster
+
+	close          func() error
+	heartbeat      chan struct{}
+	closeElection  chan struct{}
+	electionCalled bool
+}
+
+func (n *Node) stop() error {
+	return n.close()
+}
+
 // publish should be called on the leader, and the leader then sends to the other nodes
 func (n *Node) publish(msg []byte) error {
 	// TODO - make sure this is the leader
@@ -97,7 +171,7 @@ func (n *Node) publish(msg []byte) error {
 			debug("going to publish to :%d (id %d)", node.Port, node.ID)
 			// TODO - pass a `message` including term and such
 
-			client, err := rpc.Dial("tcp", fmt.Sprintf("0.0.0.0:%d", node.Port, node.ID))
+			client, err := rpc.Dial("tcp", fmt.Sprintf("0.0.0.0:%d", node.Port))
 			if err != nil {
 				log.Println("unable to dial client ", err)
 				return
@@ -121,40 +195,209 @@ func (n *Node) publish(msg []byte) error {
 // AppendEntry is the RPC call the leader will make
 func (n *Node) AppendEntry(msg LogEntry, reply *Reply) error {
 	debug("node %d AppendEntry %q", n.ID, string(msg.Data))
-	// TODO: lock
+	n.heartbeat <- struct{}{}
 
-	n.Log = append(n.Log, msg)
+	// n.mtx.Lock()
+	// defer n.mtx.Unlock()
+
+	if msg.Term > n.CurrentTerm {
+		n.CurrentTerm = msg.Term
+		// need lock
+		debug("node %d reverting to follower state", n.ID)
+		n.State = FollowerState
+	}
+
+	if msg.Term < n.CurrentTerm {
+		// ignore request
+		debug("append entry ignored, old term")
+		return nil
+	}
+
+	// if data in nil, then it is only a heartbeat
+	if msg.Data != nil {
+		n.Log = append(n.Log, msg)
+	}
 	reply.Applied = true
+	return nil
+}
+
+// Vote is supplied by each node during an election
+type Vote struct {
+	Term         int
+	CandidateID  int
+	LastLogIndex int
+	LastLogTerm  int
+}
+
+// VoteReply is returned to candidates
+type VoteReply struct {
+	Term        int
+	VoteGranted bool
+}
+
+// RequestVote is the rpc call for requesting a vote
+func (n *Node) RequestVote(vote Vote, reply *VoteReply) error {
+	n.mtx.Lock()
+	defer n.mtx.Unlock()
+
+	reply.Term = n.CurrentTerm
+	if vote.Term > n.CurrentTerm {
+		n.VotedFor = NullVote
+	}
+	if vote.Term < n.CurrentTerm || vote.LastLogIndex < n.CommitIndex || n.VotedFor == NullVote {
+		reply.VoteGranted = false
+	} else if n.VotedFor == NullVote {
+		n.VotedFor = vote.CandidateID
+		reply.VoteGranted = true
+	} else {
+		log.Println("hanging chad - how did this happen?")
+	}
+
+	debug("node %d got request vote from %d, granted %v", n.ID, vote.CandidateID, reply.VoteGranted)
+	return nil
+}
+
+func (n *Node) callElection() error {
+	n.mtx.Lock()
+	defer n.mtx.Unlock()
+
+	n.State = CandidateState
+	n.electionCalled = true
+	tally := make(chan struct{})
+	votes := 0
+	majority := determineMajority(len(n.Cluster.Nodes))
+	n.closeElection = make(chan struct{})
+
+	for _, node := range n.Cluster.Nodes {
+		go func(node *Node) {
+			debug("node %d calling an election to %d ...", n.ID, node.ID)
+			client, err := rpc.Dial("tcp", fmt.Sprintf("0.0.0.0:%d", node.Port))
+			if err != nil {
+				log.Println("unable to dial client ", err)
+				return
+			}
+
+			vote := Vote{
+				Term:         n.CurrentTerm,
+				CandidateID:  n.ID,
+				LastLogIndex: n.CommitIndex,
+				LastLogTerm:  n.CurrentTerm,
+			}
+			reply := VoteReply{}
+
+			err = client.Call("Node.RequestVote", vote, &reply)
+			if err != nil {
+				log.Println("unable to call client ", err)
+			}
+
+			if reply.VoteGranted {
+				tally <- struct{}{}
+			}
+		}(node)
+	}
+
+	timeout := time.After(ElectionTimeout)
+
+	for {
+		select {
+		case <-tally:
+			votes++
+			if votes >= majority {
+				debug("node %d is now leader", n.ID)
+				n.State = LeaderState
+				n.CurrentTerm++
+				n.CommitIndex = 0
+				n.LastApplied = 0
+				n.MatchIndex = 0
+				n.NextIndex = 1
+				// lock
+				n.Cluster.curLeader = n.ID
+
+				// TODO: not sure what to do with the error
+				err := n.sendHeartbeat()
+				if err != nil {
+					log.Println("** ERROR ** heartbeat ", err)
+				}
+				return nil
+			}
+		case <-n.closeElection:
+			debug("node %d election closed", n.ID)
+			return nil
+		case <-timeout:
+			debug("node %d election timed out", n.ID)
+			return ErrElectionTimeout
+		}
+	}
+}
+
+func (n *Node) sendHeartbeat() error {
+	debug("node %d is sending a heartbeat", n.ID)
+	data := LogEntry{}
+	data.Index = n.NextIndex
+	data.Term = n.CurrentTerm
+	data.Data = nil
+
+	reply := Reply{}
+	err := n.AppendEntry(data, &reply)
+	if err != nil {
+		log.Println("unable to send heartbeat ", err)
+		return err
+	}
+
+	if !reply.Applied {
+		return fmt.Errorf("hearbeat rejected")
+	}
+
 	return nil
 }
 
 // NewNode creates a node running on a random port
 func NewNode(ID int) (*Node, error) {
 	l, err := net.Listen("tcp", ":0")
+
 	if err != nil {
 		log.Println("unable to create listener", err)
 		return nil, err
 	}
 
 	port := addrPort(l.Addr().String())
+	heartbeat := make(chan struct{})
 
-	n := &Node{ID: ID, Port: port, Log: make([]LogEntry, 0)}
+	n := &Node{ID: ID, Port: port, Log: make([]LogEntry, 0), VotedFor: NullVote, heartbeat: heartbeat}
 
 	s := rpc.NewServer()
 	s.Register(n)
 	go s.Accept(l)
-	// rpc.Register(n)
-	// // rpc.RegisterName(fmt.Sprintf("AppendEntry_%d", ID), n)
-	// go rpc.Accept(l)
+
+	go func() {
+		for {
+			max := TriggerElectionTimeoutCap - TriggerElectionTimeoutMin
+			triggerTimeout := TriggerElectionTimeoutMin + time.Duration(rand.Intn(int(max/1e6)))*time.Millisecond
+			debug("node %d will call an election in %s if nothing comes in", ID, triggerTimeout.String())
+			select {
+			case <-heartbeat:
+				debug("node %d got a heartbeat", n.ID)
+				if n.electionCalled {
+					debug("node %d got heartbeat during election, closing election", n.ID)
+					close(n.closeElection)
+				}
+				// TODO: elections are not working
+			case <-time.After(triggerTimeout + 5*time.Second):
+				go func() {
+					debug("node %d calling an election", n.ID)
+					err := n.callElection()
+					if err == ErrElectionTimeout {
+						n.CurrentTerm++
+					}
+				}()
+			}
+		}
+
+	}()
+
+	n.close = l.Close
 
 	return n, nil
-}
-
-// LogEntry represents a raft log entry
-type LogEntry struct {
-	Term  int
-	Index int
-	Data  []byte
 }
 
 // addrPort expects a net.Addr.String() format of `[::]:9000` and will return 9000
@@ -171,4 +414,8 @@ func debug(format string, v ...interface{}) {
 	if Debug {
 		log.Printf(format, v...)
 	}
+}
+
+func determineMajority(count int) int {
+	return count/2 + 1
 }
--- raft/raft_test.go
+++ raft/raft_test.go
@@ -1,21 +1,19 @@
 package raft
 
-import (
-	"log"
-	"testing"
-)
+import "testing"
 
 func TestSend(t *testing.T) {
-	Debug = true
+	t.Skip()
+	Debug = false
 
 	clusterSize := 5
-	log.Println("test > setting up cluster")
+	debug("test > setting up cluster")
 	cluster, err := NewCluster(clusterSize)
 	if err != nil {
-		t.Fatal("unable to create new cluster", err)
+		t.Fatal("unable to create new cluster ", err)
 	}
 
-	log.Println("test > about to send...")
+	debug("test > about to send...")
 	err = cluster.Send([]byte("command 1"))
 	if err != nil {
 		t.Fatal("unable to send data to leader", err)
@@ -24,6 +22,51 @@ func TestSend(t *testing.T) {
 	for _, node := range cluster.Nodes {
 		if len(node.Log) != 1 {
 			t.Errorf("node %d got %d entry/entries, want %d", node.ID, len(node.Log), 1)
+		}
+	}
+}
+
+func TestLeaderElection(t *testing.T) {
+	Debug = true
+
+	clusterSize := 5
+	debug("test > setting up cluster")
+	cluster, err := NewCluster(clusterSize)
+	if err != nil {
+		t.Fatal("unable to create new cluster ", err)
+	}
+
+	debug("test > getting leader...")
+	leader := cluster.CurLeader()
+	originalLeaderID := leader.ID
+
+	debug("test > stopping leader...")
+	err = cluster.StopNode(originalLeaderID)
+	if err != nil {
+		t.Fatal("unable to stop node ", err)
+	}
+
+	debug("test > getting new leader...")
+	newLeader := cluster.CurLeader()
+	newLeaderID := newLeader.ID
+
+	if newLeaderID == originalLeaderID {
+		t.Errorf("Got id %d, want a different id for new leader", newLeaderID)
+	}
+}
+
+func TestDetermineMajority(t *testing.T) {
+	cases := []struct {
+		count    int
+		majority int
+	}{
+		{count: 10, majority: 6},
+		{count: 11, majority: 6},
+	}
+
+	for _, test := range cases {
+		if got, want := determineMajority(test.count), test.majority; got != want {
+			t.Errorf("got %d, want %d for majority of %d", got, want, test.count)
 		}
 	}
 }
